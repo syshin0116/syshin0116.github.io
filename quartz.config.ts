6ff44796fb5ee4fc63420b2ace3b67949c8d2263
syshin0116 <syshin0116@gmail.com>
2025-03-29 13:53:54 +0900

    Quartz sync: Mar 29, 2025, 1:53 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🖥️ Syshin",
+    pageTitle: "Syshin",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
