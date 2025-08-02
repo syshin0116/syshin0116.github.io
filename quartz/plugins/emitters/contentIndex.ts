34a51153650301c9b04c8e2ca036dd4c5609bff0
syshin0116 <syshin0116@gmail.com>
2025-08-03 01:43:12 +0900

    Quartz sync: Aug 3, 2025, 1:43 AM

--- quartz/plugins/emitters/contentIndex.ts
+++ quartz/plugins/emitters/contentIndex.ts
@@ -139,6 +139,7 @@ export const ContentIndex: QuartzEmitterPlugin<Partial<Options>> = (opts) => {
         )
         if (opts?.enableSiteMap) {
           graph.addEdge(sourcePath, joinSegments(ctx.argv.output, "sitemap.xml") as FilePath)
+          graph.addEdge(sourcePath, joinSegments(ctx.argv.output, "sitemap2.xml") as FilePath)
         }
         if (opts?.enableRSS) {
           graph.addEdge(sourcePath, joinSegments(ctx.argv.output, "index.xml") as FilePath)
@@ -175,6 +176,15 @@ export const ContentIndex: QuartzEmitterPlugin<Partial<Options>> = (opts) => {
             ctx,
             content: generateSiteMap(cfg, linkIndex),
             slug: "sitemap" as FullSlug,
+            ext: ".xml",
+          }),
+        )
+        
+        emitted.push(
+          await write({
+            ctx,
+            content: generateSiteMap(cfg, linkIndex),
+            slug: "sitemap2" as FullSlug,
             ext: ".xml",
           }),
         )
